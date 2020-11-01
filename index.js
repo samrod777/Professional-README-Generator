da997856de4cf52d0fd2216871963cbc0d436b70
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-01 13:26:47 -0800

    initial array

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // array of questions for user
-const questions = [
-
+const questions = ["Project Title: ", "Desciption: ", "Table of Contents (Optional): ", "Installation Steps: ", "How to Use it: ", "Credits: ", "License: ", "Badges: ", "How to Contribute: ", "Tests: " 
 ];
 
 // function to write README file
