b0da7974adbe38699c83fb0547dcaf217274dcca
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-02 17:38:59 -0800

    readme logic mostly complete

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Professional-README-Generator
+# Readme generator
+
+        ![screenshot]()
+        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+
+
+        ---
+        
+        ## Table of Contents
+        - [About the Project](#About-the-Project)
+        - [Getting Started](#Getting-Started)
+        - [Installation](#Installation)
+        - [Contributing](#Contributing)
+        - [Testing](#Testing)
+        - [License](#License) 
+        
+        ## About the Project
+        The user is presented with a series of questions that will be use to populate a profesional README file.
+        
+        ## Getting Started  
+        To get started, follow the Installation instructions.  
+        
+        ### Installation  
+        Run the following command in your terminal  
+            npm i
+        
+        ## Testing
+        Run the following command in your terminal  
+            npm test
+        
+        ## Contributing
+        undefined
+        
+        ## License
+        This application is covered under the MIT license;
+        
--- index.js
+++ index.js
@@ -1,15 +1,95 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // array of questions for user
-const questions = ["Project Title: ", "Desciption: ", "Table of Contents (Optional): ", "Installation Steps: ", "How to Use it: ", "Credits: ", "License: ", "Badges: ", "How to Contribute: ", "Tests: " 
-];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project's title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a short description of your project"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license should your project have?",
+            choices: ["MIT", "Apache", "GPL", "WTFPL"]
+        },
+        {
+            type: "input",
+            name: "dependencies",
+            message: "What command should be used to install required dependencies?",
+            default: "npm i"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "What command should be use to run any added tests?",
+            default: "npm test"
+        },
+        
+    ]).then((answers) => {
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+        switch (answers.license) {
+            case "MIT":
+                var mdlicense = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+                break;
+            case "Apache":
+                var mdlicense = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+                break;
+            case "WTFPL":
+                var mdlicense = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+                break;
+            default:
+                var mdlicense = "";
+                break
+        }
+    
+        const markdownString = `# ${answers.title}
 
-// function to initialize program
-function init() {
+        ![screenshot]()
+        ${mdlicense}  
 
-}
 
-// function call to initialize program
-init();
+        ---
+        
+        ## Table of Contents
+        - [About the Project](#About-the-Project)
+        - [Getting Started](#Getting-Started)
+        - [Installation](#Installation)
+        - [Contributing](#Contributing)
+        - [Testing](#Testing)
+        - [License](#License) 
+        
+        ## About the Project
+        ${answers.description}
+        
+        ## Getting Started  
+        To get started, follow the Installation instructions.  
+        
+        ### Installation  
+        Run the following command in your terminal  
+            ${answers.dependencies}
+        
+        ## Testing
+        Run the following command in your terminal  
+            ${answers.test}
+        
+        ## Contributing
+        ${answers.contribute}
+        
+        ## License
+        This application is covered under the ${answers.license} license;
+        `
+        fs.writeFile("README.md", markdownString, err => {
+                if (err) console.log(err)
+        })
+    })
+
+    // unction call to initialize program
+    questions();
