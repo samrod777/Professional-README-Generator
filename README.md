1b6bd10a088950aa687a2aef33d49edb091003ba
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-02 19:25:46 -0800

    fixed readme file

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
-# Readme generator
-
-        ![screenshot]()
-        [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+# generator  
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)   
 
 
-        ---
-        
-        ## Table of Contents
-        - [About the Project](#About-the-Project)
-        - [Getting Started](#Getting-Started)
-        - [Installation](#Installation)
-        - [Contributing](#Contributing)
-        - [Testing](#Testing)
-        - [License](#License) 
-        
-        ## About the Project
-        The user is presented with a series of questions that will be use to populate a profesional README file.
-        
-        ## Getting Started  
-        To get started, follow the Installation instructions.  
-        
-        ### Installation  
-        Run the following command in your terminal  
-            npm i
-        
-        ## Testing
-        Run the following command in your terminal  
-            npm test
-        
-        ## Contributing
-        undefined
-        
-        ## License
-        This application is covered under the MIT license;
-        
+---
+
+## Table of Contents
+- [About the Project](#About-the-Project)
+- [Getting Started](#Getting-Started)
+- [Installation](#Installation)
+- [Contributing](#Contributing)
+- [Testing](#Testing)
+- [License](#License) 
+
+## About the Project
+cool app
+
+## Getting Started  
+To get started, follow the Installation instructions.  
+
+### Installation  
+Run the following command in your terminal  
+npm i
+
+## Testing
+Run the following command in your terminal  
+npm test
+
+## Contributing
+undefined
+
+## License
+This application is covered under the MIT license;
--- index.js
+++ index.js
@@ -3,93 +3,95 @@ const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = () =>
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "What is your project's title?"
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Please write a short description of your project"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "What kind of license should your project have?",
-            choices: ["MIT", "Apache", "GPL", "WTFPL"]
-        },
-        {
-            type: "input",
-            name: "dependencies",
-            message: "What command should be used to install required dependencies?",
-            default: "npm i"
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "What command should be use to run any added tests?",
-            default: "npm test"
-        },
-        
-    ]).then((answers) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "Apache", "GPL", "WTFPL"],
+      },
+      {
+        type: "input",
+        name: "dependencies",
+        message:
+          "What command should be used to install required dependencies?",
+        default: "npm i",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be use to run any added tests?",
+        default: "npm test",
+      },
+    ])
+    .then((answers) => {
+      switch (answers.license) {
+        case "MIT":
+          var mdLicImg =
+            "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+          break;
+        case "Apache":
+          var mdLicImg =
+            "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+          break;
+        case "WTFPL":
+          var mdLicImg =
+            "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+          break;
+        default:
+          var mdLicImg = "";
+          break;
+      }
 
-        switch (answers.license) {
-            case "MIT":
-                var mdLicImg = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-                break;
-            case "Apache":
-                var mdLicImg = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-                break;
-            case "WTFPL":
-                var mdLicImg = "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
-                break;
-            default:
-                var mdLicImg = "";
-                break
-        }
-    
-        const markdownString = `# ${answers.title}
-
-        ![screenshot]()
-        ${mdLicImg}  
+    const markdownString = `# ${answers.title}  
+${mdLicImg}   
 
 
-        ---
-        
-        ## Table of Contents
-        - [About the Project](#About-the-Project)
-        - [Getting Started](#Getting-Started)
-        - [Installation](#Installation)
-        - [Contributing](#Contributing)
-        - [Testing](#Testing)
-        - [License](#License) 
-        
-        ## About the Project
-        ${answers.description}
-        
-        ## Getting Started  
-        To get started, follow the Installation instructions.  
-        
-        ### Installation  
-        Run the following command in your terminal  
-            ${answers.dependencies}
-        
-        ## Testing
-        Run the following command in your terminal  
-            ${answers.test}
-        
-        ## Contributing
-        ${answers.contribute}
-        
-        ## License
-        This application is covered under the ${answers.license} license;
-        `
-        fs.writeFile("README.md", markdownString, err => {
-                if (err) console.log(err)
-        })
-    })
+---
 
-    // function call to initialize program
-    questions();
+## Table of Contents
+- [About the Project](#About-the-Project)
+- [Getting Started](#Getting-Started)
+- [Installation](#Installation)
+- [Contributing](#Contributing)
+- [Testing](#Testing)
+- [License](#License) 
+
+## About the Project
+${answers.description}
+
+## Getting Started  
+To get started, follow the Installation instructions.  
+
+### Installation  
+Run the following command in your terminal  
+${answers.dependencies}
+
+## Testing
+Run the following command in your terminal  
+${answers.test}
+
+## Contributing
+${answers.contribute}
+
+## License
+This application is covered under the ${answers.license} license;
+`;
+      fs.writeFile("README.md", markdownString, (err) => {
+        if (err) console.log(err);
+      });
+    });
+
+// function call to initialize program
+questions();
